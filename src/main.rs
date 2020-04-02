7d473eaac2418ee9c6fb32b7648b62045486f4ef
BenSandeen <12025856+BenSandeen@users.noreply.github.com>
2020-04-02 16:50:04 -0400

    More progress

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,9 @@ fn main() {
              gl::NUM_SHADING_LANGUAGE_VERSIONS, gl::SHADING_LANGUAGE_VERSION, gl::VERSION);
     println!("Depth test: {:}", gl::DEPTH_TEST);
 
+    // let shader = Shader::from_source()
+
+
     'main: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -56,6 +59,60 @@ fn main() {
         }
 
         window.gl_swap_window();
+    }
+}
+
+// Struct to hold the shader object, simply for convenience
+struct Shader {
+    id: gl::types::GLuint,
+}
+
+impl Shader {
+    // Since this does not have a `self` parameter, this is basically a static method (doesn't act on individual objects
+    // of the `Shader` type, but rather works only with the struct itself.  This is basically a constructor method
+    fn from_source(source: &CStr, shader_type: gl::types::GLenum) -> Result<Shader, String> {
+        // The `?` does sort of the same thing as a match statement that checks for errors
+        let id = shader_from_source(source, shader_type)?;
+        Ok(Shader { id })
+    }
+
+    fn from_vert_source(source: &CStr) -> Result<Shader, String> {
+        let id = shader_from_source(source, gl::VERTEX_SHADER)?;
+        Ok(Shader { id })
+    }
+
+    fn from_frag_source(source: &CStr) -> Result<Shader, String> {
+        let id = shader_from_source(source, gl::FRAGMENT_SHADER)?;
+        Ok(Shader { id })
+    }
+
+    fn from_compute_source(source: &CStr) -> Result<Shader, String> {
+        let id = shader_from_source(source, gl::COMPUTE_SHADER)?;
+        Ok(Shader { id })
+    }
+
+    fn from_tess_control_source(source: &CStr) -> Result<Shader, String> {
+        let id = shader_from_source(source, gl::TESS_CONTROL_SHADER)?;
+        Ok(Shader { id })
+    }
+
+    fn from_tess_evaluation_source(source: &CStr) -> Result<Shader, String> {
+        let id = shader_from_source(source, gl::TESS_EVALUATION_SHADER)?;
+        Ok(Shader { id })
+    }
+
+    fn from_geometry_source(source: &CStr) -> Result<Shader, String> {
+        let id = shader_from_source(source, gl::GEOMETRY_SHADER)?;
+        Ok(Shader { id })
+    }
+}
+
+// Implement `Drop` so that wee aren't leaking memory every time a shader goes out of scope
+impl Drop for Shader {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteShader(self.id);
+        }
     }
 }
 
@@ -86,19 +143,7 @@ fn shader_from_source(source: &CStr, shader_type: gl::types::GLuint) -> Result<g
             gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
         }
 
-        // Then we allocate a vector to act as a buffer to hold the message
-        let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
-
-        // Fill the buffer with spaces, I guess
-        buffer.extend(
-            [b' ']   // "a single-item stack-allocated array which contains ASCII 'space' byte"
-                .iter()   // Obtains an iterator over the array with a single space
-                .cycle()  // Cycles over the iterator forever, yielding an infinite number of spaces
-                .take(len as usize)  // Limits number of returned items to `len`
-        );
-
-        // Convert buffer to a `CString`
-        let error: CString = unsafe { CString::from_vec_unchecked(buffer) };
+        let error = create_whitespace_cstring_with_len(len as usize);
 
         // Now that we have a buffer of the correct length and type, ask OpenGL to fill it with the message
         unsafe {
@@ -118,4 +163,19 @@ fn shader_from_source(source: &CStr, shader_type: gl::types::GLuint) -> Result<g
     Ok(id)
 }
 
+fn create_whitespace_cstring_with_len(len: usize) -> CString {
+    // Then we allocate a vector to act as a buffer to hold the message
+    let mut buffer: Vec<u8> = Vec::with_capacity(len + 1);
+
+    // Fill the buffer with spaces, I guess
+    buffer.extend(
+        [b' ']   // "a single-item stack-allocated array which contains ASCII 'space' byte"
+            .iter()   // Obtains an iterator over the array with a single space
+            .cycle()  // Cycles over the iterator forever, yielding an infinite number of spaces
+            .take(len as usize)  // Limits number of returned items to `len`
+    );
+
+    // Convert buffer to a `CString` and return it
+    unsafe { CString::from_vec_unchecked(buffer) }
+}
 
